ddebc9a81f23fa49a9cc6243cef2a51b7d660d62
sasi <sasibhushan.m@gmail.com>
2015-12-13 19:38:21 +0530

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,5 +1,5 @@
-dataFile <- "./data/household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+data <- read.table("C://users//sasimsb//Desktop//exdata-data-household_power_consumption//assign.txt",
+            header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 #str(subSetData)
