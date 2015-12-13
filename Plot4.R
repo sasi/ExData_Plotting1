cbd6bbfda66a2679ee52f78f2c7a2a5913095d2d
sasi <sasibhushan.m@gmail.com>
2015-12-13 19:42:09 +0530

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,16 +1,16 @@
-dataFile <- "./data/household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+data <- read.table("C://users//sasimsb//Desktop//exdata-data-household_power_consumption//assign.txt",
+            header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(subSetData$Global_active_power)
 globalReactivePower <- as.numeric(subSetData$Global_reactive_power)
+
 voltage <- as.numeric(subSetData$Voltage)
 subMetering1 <- as.numeric(subSetData$Sub_metering_1)
 subMetering2 <- as.numeric(subSetData$Sub_metering_2)
 subMetering3 <- as.numeric(subSetData$Sub_metering_3)
-
 
 png("plot4.png", width=480, height=480)
 par(mfrow = c(2, 2)) 
